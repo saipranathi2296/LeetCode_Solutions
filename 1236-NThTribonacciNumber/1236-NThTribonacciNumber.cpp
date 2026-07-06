e468efcff8aec66aadecfe9e7ea9d66b4635a4fd
saipranathi2296 <162856208+saipranathi2296@users.noreply.github.com>
2026-07-06 12:07:58 +0530

    Create 1236-NThTribonacciNumber

---
+++ 1236-NThTribonacciNumber/1236-NThTribonacciNumber.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/6/2026, 12:07:59 PM
+class Solution {
+public:
+    int tribonacci(int n) {
+         if(n==0)
+        {
+            return 0;
+        }
+        if(n<=2)
+        {
+            return 1;
+        }
+        vector<int> dp(n + 1);
+        dp[0]=0;
+        dp[1] = 1;
+        dp[2]=1;
+        
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        
+        return dp[n];
+    }
+};
