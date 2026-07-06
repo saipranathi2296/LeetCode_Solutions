ca5eb81c1aa59713d031374399fe3b4f841be109
saipranathi2296 <162856208+saipranathi2296@users.noreply.github.com>
2026-07-06 12:09:14 +0530

    Create 295-FindMedianFromDataStream

---
+++ 295-FindMedianFromDataStream/295-FindMedianFromDataStream.cpp
@@ -0,0 +1,37 @@
+// Last updated: 7/6/2026, 12:09:14 PM
+class MedianFinder {
+public:
+priority_queue<int> left;
+priority_queue<int,vector<int>,greater<int>> right;
+
+
+    MedianFinder() {
+      
+        
+    }
+    
+    void addNum(int num) {
+left.push(num);
+right.push(left.top());
+left.pop();
+if(right.size()>left.size())
+{
+    left.push(right.top());
+    right.pop();
+}
+        
+    }
+    
+    double findMedian() {
+        if(left.size()==right.size())
+        {
+            return (left.top()+right.top())/2.0;
+
+
+        }
+        else
+        {
+            return left.top();
+        }
+    }
+};
