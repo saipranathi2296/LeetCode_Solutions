2e87aa3f1526188f040c4149c44fce60b6a2a016
saipranathi2296 <162856208+saipranathi2296@users.noreply.github.com>
2026-07-06 12:08:20 +0530

    Create 908-MiddleOfTheLinkedList

---
+++ 908-MiddleOfTheLinkedList/908-MiddleOfTheLinkedList.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/6/2026, 12:08:21 PM
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
