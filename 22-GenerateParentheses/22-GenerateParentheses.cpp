416d5ca0104274ef0595b03c97881ec97aaa1954
saipranathi2296 <162856208+saipranathi2296@users.noreply.github.com>
2026-07-06 12:11:40 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/6/2026, 12:11:41 PM
+class Solution {
+public:
+    void solve(int open, int close, int n, string curr, vector<string>& ans) {
+        // base case
+        if (curr.length() == 2 * n) {
+            ans.push_back(curr);
+            return;
+        }
+
+        // add '(' if possible
+        if (open < n) {
+            solve(open + 1, close, n, curr + "(", ans);
+        }
+
+        // add ')' if valid
+        if (close < open) {
+            solve(open, close + 1, n, curr + ")", ans);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        solve(0, 0, n, "", ans);
+        return ans;
+    }
+};
