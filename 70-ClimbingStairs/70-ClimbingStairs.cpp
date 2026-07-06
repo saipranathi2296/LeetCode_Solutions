66d9f78cfa5790a8cc58128f841883e0f9ffb67e
saipranathi2296 <162856208+saipranathi2296@users.noreply.github.com>
2026-07-06 12:11:11 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/6/2026, 12:11:12 PM
+class Solution {
+public:
+
+    int climbStairs(int n) {
+      vector<int>dp(n+1,-1);
+      dp[0]=1;
+      dp[1]=1;
+      for(int i=2;i<=n;i++)
+      {
+        dp[i]=dp[i-1]+dp[i-2];
+
+      }
+       
+return dp[n];
+    }
+};
