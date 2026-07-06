163bda921746ccafc31e44e820efdec9727db1b4
saipranathi2296 <162856208+saipranathi2296@users.noreply.github.com>
2026-07-06 12:09:21 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/6/2026, 12:09:22 PM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.length() != t.length())
+            return false;
+        
+        int freq[26] = {0};
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        
+        for(int i = 0; i < 26; i++)
+        {
+            if(freq[i] != 0)
+                return false;
+        }
+        
+        return true;
+    }
+};
